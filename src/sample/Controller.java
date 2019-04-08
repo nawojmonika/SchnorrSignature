c2f3a40e59def485e21385bc128974be87c2d0fb
nawojmonika <nawojmonika@gmail.com>
2019-04-08 22:24:04 +0200

    Added generating private key

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 public class Controller {
+    public BigInteger generatePrivateKey(BigInteger q){
+        return getNextRandValue(q);
+    }
+
+    private BigInteger getNextRandValue(BigInteger val){
+        BigInteger result = val;
+        while (result.compareTo(val) >= 0){
+            Random rand = new Random();
+            result = new BigInteger(val.bitLength(), rand);
+        }
+        return result;
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,8 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.math.BigInteger;
 
+public class Main extends Application {
+    public BigInteger p; //prime number
+    public BigInteger q; //factor of p-1
+    public BigInteger a; // a^q = 1modp
+    private BigInteger s; // Private key
+    private BigInteger v; // Public key
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
