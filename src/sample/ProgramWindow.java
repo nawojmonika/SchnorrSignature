3ec1c80ba33e890e5554fe190928945295283f5d
nawojmonika <nawojmonika@gmail.com>
2019-04-24 21:17:13 +0200

    Created grid panel with textarea

--- src/sample/ProgramWindow.java
+++ src/sample/ProgramWindow.java
@@ -1,23 +1,28 @@
 package sample;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 
 public class ProgramWindow {
     private static BorderPane borderPane = null;
+    private static TextArea textarea = null;
+    private static Button actionButton = null;
 
     ProgramWindow(Stage mainStage){
         mainStage.setTitle("Schnorr's signature");
         Group root = new Group();
-        Scene scene = new Scene(root, 800, 400);
+        Scene scene = new Scene(root, 700, 400);
         this.borderPane = new BorderPane();
+        this.borderPane.setPrefSize(700, 400);
         this.setupMenu();
+        this.setupInputs();
         root.getChildren().add(this.borderPane);
         mainStage.setScene(scene);
         mainStage.show();
@@ -40,11 +45,23 @@ public class ProgramWindow {
         file.getItems().addAll(readMessage, importConfig, exportConfig, readSign);
 
         menuBar.getMenus().addAll(file, message);
-        borderPane.setTop(menuBar);
+        this.borderPane.setTop(menuBar);
     }
 
     private void setupInputs(){
+        GridPane inputsGrid = new GridPane();
+        inputsGrid.setPrefSize(700, 400);
+        inputsGrid.setPadding(new Insets(50,150,50,150));
+        inputsGrid.setVgap(10);
 
+        this.textarea = new TextArea();
+        this.textarea.setPrefSize(400, 150);
+        inputsGrid.add(textarea,1, 1);
 
+        this.actionButton = new Button("Sign");
+        inputsGrid.add(actionButton, 1, 2);
+        GridPane.setHalignment(this.actionButton, HPos.RIGHT);
+
+        this.borderPane.setCenter(inputsGrid);
     }
 }
